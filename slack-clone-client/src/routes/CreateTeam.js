ee7ebe44ec384c15ee8dc6e0b0fbde216c2e13b7
Erce Kalabalikoglu <erce@Spare3s-MacBook-Pro.local>
2018-05-10 17:06:07 +0100

    adds route protection through jwt check

--- slack-clone-client/src/routes/CreateTeam.js
+++ slack-clone-client/src/routes/CreateTeam.js
@@ -22,11 +22,18 @@ class CreateTeam extends React.Component {
 
   onSubmit = async () => {
     const { name } = this
-    const response = await this.props.mutate({
-      variables: { name }
-    })
+    let response = null;
+
+    try {
+      response = await this.props.mutate({
+        variables: { name }
+      })
+    } catch (err) {
+      this.props.history.push('/login')
+      return
+    }
+    console.log(response);
     const {ok, errors} = response.data.createTeam
-    console.log(response.data.createTeam);
     if (ok) {
       this.props.history.push('/')
     } else {
