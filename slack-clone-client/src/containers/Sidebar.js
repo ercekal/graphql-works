2dc001497d83b0e0ea5f07c9e7679edd0f134eec
Erce Kalabalikoglu <erce@Spare3s-MacBook-Pro.local>
2018-05-14 16:37:28 +0100

    builds container for teams and channel

---
+++ slack-clone-client/src/containers/Sidebar.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import _ from 'lodash'
+import decode from 'jwt-decode';
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag';
+import Channels from '../components/Channels'
+import Teams from '../components/Teams'
+
+const Sidebar = ({data: {loading, allTeams}, currentTeamId }) =>{
+  if (loading) {
+    return null
+  }
+
+  const teamIdx = _.findIndex(allTeams, ['id', currentTeamId])
+  const team = allTeams[teamIdx]
+  let username = ''
+  try {
+    const token = localStorage.getItem('token')
+    const { user } = decode(token)
+    username = user.username
+  } catch (err) {}
+  return [
+    <Teams
+      key='teams-sidebar'
+      teams={allTeams.map(t => ({
+        id: t.id,
+        letter: t.name.charAt(0).toUpperCase(),
+      }))}
+    />,
+    <Channels
+      key='channels-sidebar'
+      teamName={team.name}
+      userName={username}
+      channels={team.channels}
+      users={[{id: 1, name: 'slackbot'}, {id: 2, name: 'user2'}]}
+    />,
+  ]
+};
+
+const allTeamsQuery = gql`
+  {
+    allTeams {
+      id
+      name
+      channels {
+        id
+        name
+      }
+    }
+  }
+`
+
+export default graphql(allTeamsQuery)(Sidebar)
--- slack-clone-client/src/routes/ViewTeam.js
+++ slack-clone-client/src/routes/ViewTeam.js
@@ -1,23 +1,14 @@
 
 import React from 'react';
-import Channels from '../components/Channels'
-import Teams from '../components/Teams'
 import Messages from '../components/Messages'
 import SendMessage from '../components/SendMessage'
 import Header from '../components/Header'
+import Sidebar from '../containers/Sidebar'
 import AppLayout from '../components/AppLayout'
 
 export default () => (
   <AppLayout>
-    <Teams
-      teams={[{ id: 1, letter: 'T' }, { id: 2, letter: 'E' }]}
-    />
-    <Channels
-      teamName='Team Name'
-      userName='Username'
-      channels={[{id: 1, name: 'general'}, {id: 2, name: 'random'}]}
-      users={[{id: 1, name: 'slackbot'}, {id: 2, name: 'user2'}]}
-    />
+    <Sidebar currentTeamId={1}/>
     <Header channelName='general'/>
     <Messages>
       <ul class="message-list">
