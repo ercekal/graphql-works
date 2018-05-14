9eb8c5b7f47082cb3c4edc7af4f625d1864ee9d5
Erce Kalabalikoglu <erce@Spare3s-MacBook-Pro.local>
2018-05-14 13:02:54 +0100

    styling works

--- slack-clone-client/src/routes/ViewTeam.js
+++ slack-clone-client/src/routes/ViewTeam.js
@@ -3,28 +3,28 @@ import React from 'react';
 import Channels from '../components/Channels'
 import Teams from '../components/Teams'
 import Messages from '../components/Messages'
-import Input from '../components/Input'
+import SendMessage from '../components/SendMessage'
 import Header from '../components/Header'
 import AppLayout from '../components/AppLayout'
 
 export default () => (
   <AppLayout>
-    <Teams>Teams</Teams>
+    <Teams
+      teams={[{ id: 1, letter: 'T' }, { id: 2, letter: 'E' }]}
+    />
     <Channels
       teamName='Team Name'
       userName='Username'
       channels={[{id: 1, name: 'general'}, {id: 2, name: 'random'}]}
       users={[{id: 1, name: 'slackbot'}, {id: 2, name: 'user2'}]}
     />
-    <Header>Header</Header>
+    <Header channelName='general'/>
     <Messages>
       <ul class="message-list">
         <li></li>
         <li></li>
       </ul>
     </Messages>
-    <Input>
-      <input type="text" placeholder="CSS Grid Layout Module" />>
-    </Input>
+    <SendMessage channelName='general'/>
   </AppLayout>
 )
